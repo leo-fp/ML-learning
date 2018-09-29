1a6f9b9f1455c12a5814cabb09a0249db14d82e2
leo-fp <2491587009@qq.com>
2018-09-29 23:32:55 +0800

    添加数据集和Logistic回归最佳拟合直线的绘制函数

--- content/logRegres.py
+++ content/logRegres.py
@@ -44,3 +44,27 @@ def gradAscent(dataMatIn,classLabels):
         error = (labelMat - h)
         weights = weights + alpha * dataMatrix.transpose() * error
     return weights
+
+#画出数据集和Logistic回归最佳拟合直线
+def plotBestFit(wei):
+    import matplotlib.pyplot as plt
+    weights = wei
+    dataMat,labelMat = loadDataSet()
+    dataArr = np.array(dataMat)
+    n = np.shape(dataArr)[0]
+    xcord1 = []; ycord1 = []
+    xcord2 = []; ycord2 = []
+    for i in range(n):
+        if int(labelMat[i]) == 1:
+            xcord1.append(dataArr[i,1]);ycord1.append(dataArr[i,2])
+        else:
+            xcord2.append(dataArr[i,1]);ycord2.append(dataArr[i,2])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xcord1,ycord1,s = 30,c = "red",marker = "s")
+    ax.scatter(xcord2,ycord2,s = 30,c = "green")
+    x = np.arange(-3.0,3.0,0.1)
+    y = (-weights[0] - weights[1] * x) / weights[2]
+    ax.plot(x,y)
+    plt.xlabel("x1");plt.ylabel("x2");
+    plt.show()
